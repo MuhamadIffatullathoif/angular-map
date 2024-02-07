442b4a03ef2e0329ea8c26d0610153ebe001c64e
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-07 07:45:18 +0700

    03. Show a loading message

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {PlacesService} from "../../services";
 
 @Component({
@@ -8,5 +8,9 @@ import {PlacesService} from "../../services";
 })
 export class MapScreenComponent {
   constructor(private placesService: PlacesService) {
+  }
+
+  get isUserLocationReady(): boolean {
+    return this.placesService.isUserLocationReady;
   }
 }
